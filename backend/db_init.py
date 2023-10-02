7ea61df13b1d10bc00c6a3bd68c425bc8715b8c3
NidhayPancholi <nidhay2110@gmail.com>
2023-10-02 12:02:28 -0400

    Update db_init.py

--- backend/db_init.py
+++ backend/db_init.py
@@ -1,5 +1,6 @@
 import pymongo
 import ssl
+#added my own id and password
 uri = "mongodb+srv://nrpancho:Nidhay2110@cluster0.wwokmcs.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(uri,ssl_cert_reqs=ssl.CERT_NONE)
 db = client.get_database('feature-hunt')
